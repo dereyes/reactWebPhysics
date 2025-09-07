2d851010eb5fcf7df38fa01757588b75638bd13b
Darin Reyes <hello@derey.es>
2025-09-07 14:48:21 -0700

    Drop a ball on click

--- src/components/PhysFruit/PhysFruit.jsx
+++ src/components/PhysFruit/PhysFruit.jsx
@@ -1,11 +1,36 @@
-import { RigidBody } from "@react-three/rapier";
-export function PhysFruit() {
+import { RigidBody, BallCollider } from "@react-three/rapier";
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+
+export function PhysFruit({ isDropped }) {
+  const rigidBodyRef = useRef();
+
+  // Update the square's position when in 'follow' mode
+  useFrame(({ pointer }) => {
+    if (!isDropped) {
+      rigidBodyRef.current.setNextKinematicTranslation(
+        {
+          x: pointer.x * 5,
+          y: 4,
+          z: 0,
+        },
+        true
+      );
+    }
+  });
+
   return (
-    <RigidBody mass={1} position={[0, 5, 0]}>
+    <RigidBody
+      ref={rigidBodyRef}
+      mass={1}
+      position={[0, 4, 0]}
+      type={isDropped ? "dynamic" : "kinematicPosition"}
+    >
       <mesh>
-        <boxGeometry args={[1, 1, 1]} />
+        <circleGeometry args={[0.5, 32]} />
         <meshBasicMaterial color="hotpink" />
       </mesh>
+      <BallCollider args={[0.5]} />
     </RigidBody>
   );
 }
